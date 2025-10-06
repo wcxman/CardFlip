82ba85431e1cf0fa3df87c955b58f5a3754ecc52
wcxman <christman.w@northeastern.edu>
2025-10-06 12:42:13 -0400

    Started card constructor

--- 10-wuchenchr-2a/card.cpp
+++ 10-wuchenchr-2a/card.cpp
@@ -1,1 +1,39 @@
-//Card cpp file
+#include "card.h"
+#include <stdexcept>
+
+class card
+{
+private:
+	char suit;
+	char val;
+
+public:
+	//Constructor 
+	card(char card_suit, char card_value)
+	{
+		switch (card_suit) {
+		case 'h':
+		case 'H':
+			suit = 'H';
+			break;
+
+		case 'd':
+		case 'D':
+			suit = 'D';
+			break;
+
+		case 'c':
+		case 'C':
+			suit = 'C';
+			break;
+
+		case 's':
+		case 'S':
+			suit = 'S';
+			break;
+		default:
+			throw std::invalid_argument("Invalid suit character");
+		}
+		//TO IMPLEMENT: Card Value switch case
+	}
+};
--- 10-wuchenchr-2a/card.h
+++ 10-wuchenchr-2a/card.h
@@ -1,1 +1,23 @@
 //card header
+#ifndef CARD_H
+#define CARD_H
+#include <stdexcept>
+
+
+
+class card
+{
+	private:
+		char suit;
+		char val;
+
+	public: 
+		//Constructor 
+		card(char card_suit, char card_value);
+
+};
+
+
+
+
+#endif
